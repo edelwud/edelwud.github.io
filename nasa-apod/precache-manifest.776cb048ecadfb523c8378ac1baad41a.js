48cf04931a65afac831a4e4baaaba2d8ae3418bb
Max Ersh <edelwud@icloud.com>
2020-06-21 20:28:45 +0300

    Nasa APOD Service

---
+++ nasa-apod/precache-manifest.776cb048ecadfb523c8378ac1baad41a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af59c42ab28d50838ff9a2cc1bc22d28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b19c88d20e54d283778a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "519af673a3292986f31c",
+    "url": "/static/css/main.74417940.chunk.css"
+  },
+  {
+    "revision": "b19c88d20e54d283778a",
+    "url": "/static/js/2.755d1d08.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.755d1d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519af673a3292986f31c",
+    "url": "/static/js/main.44899944.chunk.js"
+  },
+  {
+    "revision": "420a557a96859f92db24",
+    "url": "/static/js/runtime-main.7f670f00.js"
+  }
+]);
